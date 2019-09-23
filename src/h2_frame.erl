b1c61e8a499b4dd4b437d4361454d00191a475d6
Paul J. Davis <paul.joseph.davis@gmail.com>
2019-09-23 14:35:15 -0500

    Initial refactoring of frame handling

--- src/h2_frame.erl
+++ src/h2_frame.erl
@@ -1,274 +1,315 @@
 -module(h2_frame).
 
--include("http2.hrl").
+-include("h2.hrl").
 
 -export([
-         recv/1,
-         read/1,
-         read/2,
-         read_binary_frame_header/1,
-         read_binary_payload/2,
-         from_binary/1,
-         format_header/1,
-         format_payload/1,
-         format/1,
-         to_binary/1,
-         header_to_binary/1
+    recv/1,
+    render/1
 ]).
 
--type payload() :: h2_frame_data:payload()
-                 | h2_frame_headers:payload()
-                 | h2_frame_priority:payload()
-                 | h2_frame_rst_stream:payload()
-                 | h2_frame_settings:payload()
-                 | h2_frame_push_promise:payload()
-                 | h2_frame_ping:payload()
-                 | h2_frame_goaway:payload()
-                 | h2_frame_window_update:payload()
-                 | h2_frame_continuation:payload().
 
--type header() :: #frame_header{}.
--type frame() :: {header(),
-                  payload()}.
-
--export_type([frame/0, header/0, payload/0]).
-
-%% Each frame type should be able to be read off a binary stream. If
-%% the header is good, then it'll know how many more bytes to take off
-%% the stream for the payload. If there are more bytes left over, it's
-%% the next header, so we should return a tuple that contains the
-%% remainder as well.
--callback read_binary(Bin::binary(),
-                      Header::header()) ->
-    {ok, payload(), Remainder::binary()}
-  | {error, stream_id(), error_code(), binary()}.
-
-%% For io:formating
--callback format(payload()) -> iodata().
-
-%% convert payload to binary
--callback to_binary(payload()) -> iodata().
-
-%% TODO: some kind of callback for sending frames
-%-callback send(port(), payload()) -> ok | {error, term()}.
-
--spec recv(binary() |
-           {header(), binary()}) ->
-                  {ok, frame(), binary()}
-                | {not_enough_header, binary()}
-                | {not_enough_payload, header(), binary()}
-                | {error, stream_id(), error_code(), binary()}.
-recv(Bin)
-  when is_binary(Bin), byte_size(Bin) < 9 ->
-    {not_enough_header, Bin};
-recv(Bin)
-  when is_binary(Bin) ->
-    {Header, PayloadBin} = read_binary_frame_header(Bin),
-    recv({Header, PayloadBin});
-recv({Header, PayloadBin})
-  when byte_size(PayloadBin) < Header#frame_header.length ->
-    {not_enough_payload, Header, PayloadBin};
-recv({Header, PayloadBin}) ->
-    case read_binary_payload(PayloadBin, Header) of
-        {ok, Payload, Rem} ->
-            {ok, {Header, Payload}, Rem};
-        Error -> Error
+-spec recv(binary() | {frame(), binary()}) ->
+        {ok, frame(), binary()} |
+        {more, binary()} |
+        {more, header(), binary()} |
+        {error, stream_id(), error_code(), binary()}.
+recv(Bin) when is_binary(Bin), byte_size(Bin) < 9 ->
+    {more, Bin};
+recv(Bin) when is_binary(Bin) ->
+    {InitFrame, RestBin} = recv_frame_header(Bin),
+    recv({InitFrame, RestBin});
+recv({Frame, Bin}) when byte_size(Bin) < Frame#frame.length ->
+    {more, Frame, Bin};
+recv({#frame{type = Type} = Frame, Bin}) ->
+    case Type of
+        ?DATA -> recv_data(Frame, Bin);
+        ?HEADERS -> recv_headers(Frame, Bin);
+        ?PRIORITY -> recv_priority(Frame, Bin);
+        ?RST_STREAM -> recv_rst_stream(Frame, Bin);
+        ?SETTINGS -> recv_settings(Frame, Bin);
+        ?PUSH_PROMISE -> recv_push_promise(Frame, Bin);
+        ?PING -> recv_ping(Frame, Bin);
+        ?GOAWAY -> recv_goaway(Frame, Bin);
+        ?WINDOW_UPDATE -> recv_window_update(Frame, Bin);
+        ?CONTINUATION -> recv_continuation(Frame, Bin);
+        _Unknown -> recv_unknown(Frame, Bin)
     end.
 
--spec read(socket()) -> frame().
-read(Socket) ->
-    read(Socket, infinity).
 
--spec read(socket(), timeout()) -> frame() | {error, closed|inet:posix()}.
-read(Socket, Timeout) ->
-    case read_header(Socket, Timeout) of
-        {error, Reason} ->
-            {error, Reason};
-        FrameHeader ->
-            {ok, Payload} = read_payload(Socket, FrameHeader, Timeout),
-            {FrameHeader, Payload}
-    end.
+-spec send(any(), h2:frame()) -> ok | {error, inet:posix()}.
+render(#frame{} = Frame) ->
+    render([Frame]);
 
-%% Hi, it's been a while. We need to massage the API here into
-%% something that can handle an unknown number of bytes in a binary
-%% containing an unknown quantity of frames
+render(Frames) when is_list(Frames) ->
+    IoData = lists:map(fun(Frame) ->
+        render_frame(Frame)
+    end, Frames),
+    sock:send(Socket, IoData).
 
--spec from_binary(binary()) -> [frame()].
-from_binary(Bin) ->
-    from_binary(Bin, []).
 
-from_binary(<<>>, Acc) ->
-    Acc;
-from_binary(Bin, Acc) ->
-    {Header, PayloadBin} = read_binary_frame_header(Bin),
-    {ok, Payload, Rem} = read_binary_payload(PayloadBin, Header),
-    from_binary(Rem, [{Header, Payload}|Acc]).
-
--spec format_header(header()) -> iodata().
-format_header(#frame_header{
-        length = Length,
-        type = Type,
-        flags = Flags,
-        stream_id = StreamId
-    }) ->
-    io_lib:format("[Frame Header: L:~p, T:~p, F:~p, StrId:~p]", [Length, ?FT(Type), Flags, StreamId]).
-
--spec read_header(socket(), timeout()) -> header() | {error, closed|inet:posix()}.
-read_header({Transport, Socket}, Timeout) ->
-    case Transport:recv(Socket, 9, Timeout) of
-        {ok, HeaderBytes} ->
-            {Header, <<>>} = read_binary_frame_header(HeaderBytes),
-            Header;
-        E -> E
-    end.
-
--spec read_binary_frame_header(binary()) -> {header(), binary()}.
-read_binary_frame_header(<<Length:24,Type:8,Flags:8,_R:1,StreamId:31,Rem/bits>>) ->
-    Header = #frame_header{
+recv_frame_header(Bin) ->
+    <<
+        Length:24,
+        Type:8,
+        Flags:8,
+        _R:1,
+        StreamId:31,
+        Rest/binary
+    >> = Bin,
+    Frame = #frame{
         length = Length,
         type = Type,
         flags = Flags,
         stream_id = StreamId
     },
-    {Header, Rem}.
+    {Frame, Rest}.
 
--spec read_payload(socket(), header(), timeout()) ->
-    {ok, payload()} | {error, closed|inet:posix()}.
-read_payload(_, Header=#frame_header{length=0}, _Timeout) ->
-    {ok, FramePayload, <<>>} = read_binary_payload(<<>>, Header),
-    {ok, FramePayload};
-read_payload({Transport, Socket}, Header=#frame_header{length=L}, Timeout) ->
-    case Transport:recv(Socket, L, Timeout) of
-        {ok, DataBin} ->
-            {ok, FramePayload, <<>>} = read_binary_payload(DataBin, Header),
-            {ok, FramePayload};
-        E -> E
+
+recv_data(#frame{stream_id = 0}, _Bin) ->
+    {error, 0, ?PROTOCOL_ERROR, <<>>};
+recv_data(#frame{length = 0} = Frame, _Bin) ->
+    {ok, Frame#frame{data = <<>>}, Bin};
+recv_data(Frame, Bin) ->
+    #frame{
+        length = Length,
+        stream_id = StreamId
+    } = Frame,
+    <<MaybePadded:Length/binary, Rest/binary>> = Bin,
+    case h2_padding:depad(Frame, MaybePadded) of
+        {ok, Data} ->
+            {ok, Frame#frame{data = Data}, Rest}
+        {error, Code} ->
+            {error, StreamId, Code, Rest}
     end.
 
--spec read_binary_payload(binary(), header()) ->
-                                 {ok, payload(), binary()}
-                               | {error, error_code()}
-                               | {error, stream_id(), error_code(), binary()}.
-read_binary_payload(Bin, Header = #frame_header{type=?DATA}) ->
-    h2_frame_data:read_binary(Bin, Header);
-read_binary_payload(Bin, Header = #frame_header{type=?HEADERS}) ->
-    h2_frame_headers:read_binary(Bin, Header);
-read_binary_payload(Bin, Header = #frame_header{type=?PRIORITY}) ->
-    h2_frame_priority:read_binary(Bin, Header);
-read_binary_payload(Bin, Header = #frame_header{type=?RST_STREAM}) ->
-    h2_frame_rst_stream:read_binary(Bin, Header);
-read_binary_payload(Bin, Header = #frame_header{type=?SETTINGS}) ->
-    h2_frame_settings:read_binary(Bin, Header);
-read_binary_payload(Bin, Header = #frame_header{type=?PUSH_PROMISE}) ->
-    h2_frame_push_promise:read_binary(Bin, Header);
-read_binary_payload(Bin, Header = #frame_header{type=?PING}) ->
-    h2_frame_ping:read_binary(Bin, Header);
-read_binary_payload(Bin, Header = #frame_header{type=?GOAWAY}) ->
-    h2_frame_goaway:read_binary(Bin, Header);
-read_binary_payload(Bin, Header = #frame_header{type=?WINDOW_UPDATE}) ->
-    h2_frame_window_update:read_binary(Bin, Header);
-read_binary_payload(Bin, Header = #frame_header{type=?CONTINUATION}) ->
-    h2_frame_continuation:read_binary(Bin, Header);
-read_binary_payload(Bin, Header) ->
-    read_unsupported_frame_binary(Bin, Header).
 
-read_unsupported_frame_binary(Bin,
-                              #frame_header{length=0}) ->
-    {ok, <<>>, Bin};
-read_unsupported_frame_binary(Bin,
-                              #frame_header{length=L}) ->
-    <<PayloadBin:L/binary,Rem/bits>> = Bin,
-    {ok, PayloadBin, Rem}.
-
--spec format_payload(frame()) -> iodata().
-format_payload({#frame_header{type=?DATA}, P}) ->
-    h2_frame_data:format(P);
-format_payload({#frame_header{type=?HEADERS}, P}) ->
-    h2_frame_headers:format(P);
-format_payload({#frame_header{type=?PRIORITY}, P}) ->
-    h2_frame_priority:format(P);
-format_payload({#frame_header{type=?RST_STREAM}, P}) ->
-    h2_frame_rst_stream:format(P);
-format_payload({#frame_header{type=?SETTINGS}, P}) ->
-    h2_frame_settings:format(P);
-format_payload({#frame_header{type=?PUSH_PROMISE}, P}) ->
-    h2_frame_push_promise:format(P);
-format_payload({#frame_header{type=?PING}, P}) ->
-    h2_frame_ping:format(P);
-format_payload({#frame_header{type=?GOAWAY}, P}) ->
-    h2_frame_goaway:format(P);
-format_payload({#frame_header{type=?WINDOW_UPDATE}, P}) ->
-    h2_frame_window_update:format(P);
-format_payload({#frame_header{type=?CONTINUATION}, P}) ->
-    h2_frame_continuation:format(P);
-format_payload({_, _P}) ->
-    "Unsupported Frame".
+recv_headers(#frame{stream_id = 0}, _) ->
+    {error, 0, ?PROTOCOL_ERROR, <<>>};
+recv_headers(Frame, Bin) ->
+    #frame{
+        length = Length,
+        flags = Flags,
+        stream_id = StreamId
+    } = Frame,
+    <<MaybePadded:Length/binary, Rest/binary>> = Bin,
+    case h2_padding:depad(Frame, MaybePadded) of
+        {ok, Data} ->
+            {Priority, RestData} = case ?IS_FLAG(Flags, ?PRIORITY) of
+                true ->
+                    h2_priority:read(Data);
+                false ->
+                    {#priority{}, Data}
+            end,
+            {ok, Frame#frame{data = {Priority, RestData}}, Rest};
+        {error, Code} ->
+            {error, Frame#frame.stream_id, Code, Rest}
+    end.
 
 
--spec format(frame()) -> iodata().
-format(error) -> "error";
-format({error, E}) -> io_lib:format("error : ~p",[E]);
-format({Header, Payload}) ->
-    lists:flatten(io_lib:format("~s | ~s", [format_header(Header), format_payload({Header, Payload})]));
-format(<<>>) -> "".
+recv_priority(#frame{stream_id = 0}, _Bin) ->
+    {error, 0, ?PROTOCOL_ERROR, <<>>};
+recv_priority(#frame{length = Length}, Bin) when Length /= 5 ->
+    <<_:Length/binary, Rest/binary>> = Bin,
+    #frame{
+        stream_id = StreamId
+    } = Frame,
+    {error, StreamId, ?FRAME_SIZE_ERROR, Rest};
+recv_priority(Frame, Bin) ->
+    #frame{
+        stream_id =StreamId
+    } = Frame,
+    {Priority, Rest} = h2_priority:read(Bin),
+    {ok, Frame#frame{data = Priority}, Rest}.
 
--spec to_binary(frame()) -> iodata().
-to_binary({Header, Payload}) ->
-    {Type, PayloadBin} = payload_to_binary(Payload),
-    NewHeader = Header#frame_header{
-                  length = iodata_size(PayloadBin),
-                  type = Type
-                 },
-    HeaderBin = header_to_binary(NewHeader),
-    [HeaderBin, PayloadBin].
 
--spec header_to_binary(header()) -> iodata().
-header_to_binary(#frame_header{
-        length=L,
-        type=T,
-        flags=F,
-        stream_id=StreamId
-    }) ->
-    <<L:24,T:8,F:8,0:1,StreamId:31>>.
+recv_rst_stream(#frame{stream_id = 0}, _Bin) ->
+    {error, 0, ?PROTOCOL_ERROR, <<>>};
+recv_rst_stream(#frame{length = Length}, _Bin) when Length /= 4 ->
+    {error, 0, ?FRAME_SIZE_ERROR, <<>>};
+recv_rst_stream(Frame, Bin) ->
+    <<ErrorCode:32, Rest/binary>> = Bin,
+    {ok, Frame#frame{data = ErrorCode}, Rest}.
 
--spec payload_to_binary(payload()) -> {frame_type(), iodata()}.
-payload_to_binary(P) ->
-    Type = payload_type(P),
 
-    Bin =
-        case Type of
-            ?DATA -> h2_frame_data:to_binary(P);
-            ?HEADERS -> h2_frame_headers:to_binary(P);
-            ?PRIORITY -> h2_frame_priority:to_binary(P);
-            ?RST_STREAM -> h2_frame_rst_stream:to_binary(P);
-            ?SETTINGS -> h2_frame_settings:to_binary(P);
-            ?PUSH_PROMISE -> h2_frame_push_promise:to_binary(P);
-            ?PING -> h2_frame_ping:to_binary(P);
-            ?GOAWAY -> h2_frame_goaway:to_binary(P);
-            ?WINDOW_UPDATE -> h2_frame_window_update:to_binary(P);
-            ?CONTINUATION -> h2_frame_continuation:to_binary(P)
-        end,
-    {Type, Bin}.
+recv_settings(#frame{length = Length}, _Bin) when Length rem 6 /= 0 ->
+    {error, 0, ?FRAME_SIZE_ERROR, <<>>};
+recv_settings(#frame{stream_id = 0, length = 0} = Frame, Bin) ->
+    {ok, Frame#frame{data = #settings{}}, Bin};
+recv_settings(#frame{stream_id = 0} = Frame, Bin) ->
+    #frame{
+        length = Length
+    } = Frame,
+    <<Data:Length/binary, Rest/binary>> = Bin,
+    Settings = parse_settings(Data, []),
+    {ok, Frame#frame{data = Settings}, Rest};
+recv_settings(_Frame, _Bin) ->
+    % Invalid stream_id for settings frame
+    {error, 0, ?PROTOCOL_ERROR, <<>>}.
 
-iodata_size(L) when is_list(L) ->
-    lists:foldl(fun(X, Acc) ->
-                        Acc + iodata_size(X)
-                end, 0, L);
-iodata_size(B) when is_binary(B) ->
-    byte_size(B).
 
-%% Breaking my own abstraction here, but only for a performance
-%% optimization. This function assumes that records are built with the
-%% record name as the first element in the tuple, and if that changes,
-%% or if payloads are no longer records, then this will stop working.
--spec payload_type(payload()) -> frame_type().
-payload_type(P) when element(1, P) =:= data          -> ?DATA;
-payload_type(P) when element(1, P) =:= headers       -> ?HEADERS;
-payload_type(P) when element(1, P) =:= priority      -> ?PRIORITY;
-payload_type(P) when element(1, P) =:= rst_stream    -> ?RST_STREAM;
-payload_type(P) when element(1, P) =:= settings      -> ?SETTINGS;
-payload_type(P) when element(1, P) =:= push_promise  -> ?PUSH_PROMISE;
-payload_type(P) when element(1, P) =:= ping          -> ?PING;
-payload_type(P) when element(1, P) =:= goaway        -> ?GOAWAY;
-payload_type(P) when element(1, P) =:= window_update -> ?WINDOW_UPDATE;
-payload_type(P) when element(1, P) =:= continuation  -> ?CONTINUATION.
+parse_settings(<<>>, Acc) ->
+    lists:reverse(Acc);
+
+parse_settings(Bin, Acc) ->
+    <<Key:16, Value:4/binary, Rest/binary>> = Bin,
+    case lists:member(Key, ?KNOWN_SETTINGS) of
+        true ->
+            NewAcc = [{Key, binary:decode_unsigned(Value)} | Acc],
+            parse_settings(Rest, NewAcc);
+        false ->
+            parse_settings(Rest, Acc)
+    end.
+
+
+recv_push_promise(#frame{stream_id = 0}, Bin) ->
+    {error, 0, ?PROTOCOL_ERROR, <<>>};
+recv_push_promise(Frame, Bin) ->
+    #frame{
+        length = Length,
+        stream_id = StreamId
+    } = Frame,
+    <<MaybePadded:Length/binary, Rest/binary>> = Bin,
+    case h2_padding:depad(Frame, MaybePadded) of
+        {ok, Data} ->
+            <<_R:1, StreamId:31, RestData/binary>> = Data,
+            {ok, Frame#fraome{data = {StreamId, RestData}}, Rest};
+        {error, Code} ->
+            {error, StreamId, Code, Rest}
+    end.
+
+
+recv_ping(#frame{length = Length}, Bin) when Length /= 8 ->
+    {error, 0, ?FRAME_SIZE_ERROR, <<>>};
+recv_ping(#frame{stream_id = 0}, Bin) ->
+    <<Data:8/binary, Rest/binary>> = Bin,
+    {ok, Frame#frame{data = Data}, Rest};
+recv_ping(_Frame, _Bin) ->
+    % Invalid stream_id for ping
+    {error, 0, ?PROTOCOL_ERROR, <<>>}.
+
+
+recv_goaway(#frame{stream_id = 0} = Frame, Bin) ->
+    #frame{
+        length = Length
+    } = Frame,
+    <<Data:Length/binary, Rest/binary>> = Bin,
+    <<_R:1, LastStreamId:31, ErrorCode:32, Extra/binary>> = Data,
+    {ok, Frame#frame{data = {LastStreamId, ErrorCode, Debug}}, Rest};
+recv_goaway(_Frame, _Bin) ->
+    % Invalid stream_id for goaway
+    {error, 0, ?PROTOCOL_ERROR, <<>>}.
+
+
+recv_window_update(#frame{length = Lengty}, _Bin) when Length /= 4 ->
+    {error, 0, ?FRAME_SIZE_ERROR, <<>>};
+recv_window_update(Frame, <<_R:1, 0:31, Rest/binary>>) ->
+    #frame{
+        stream_id = StreamId
+    } = Frame,
+    {error, StreamId, ?PROTOCOL_ERROR, Rest};
+recv_window_update(Frame, Bin) ->
+    <<_R:1, Increment:31, Rest/binary>> = Bin,
+    {ok, Frame#frame{data = Increment}, Rest}.
+
+
+recv_continuation(#frame{stream_id = 0}, _Bin) ->
+    {error, 0, ?PROTOCOL_ERROR, <<>>};
+recv_continuation(Frame, Bin) ->
+    #frame{
+        length = Length
+    } = Frame,
+    <<Data:Length/binary, Rest/binary>> = Bin,
+    {ok, Frame#frame{data = Data}, Rest}.
+
+
+recv_unknown(Frame, Bin) ->
+    #frame{
+        length = Length
+    } = Frame,
+    <<Data:Length/binary, Rest/binary>> = Bin,
+    {ok, Frame#frame{data = Data}, Rest}.
+
+
+render_frame(Frame) ->
+    #frame{
+        type = Type,
+        flags = Flags,
+        stream_id = StreamId,
+        data = Data
+    } = Frame,
+    IoData = case Type of
+        ?DATA -> render_data(Data);
+        ?HEADERS -> render_headers(Flags, Data);
+        ?PRIORITY -> render_priority(Data);
+        ?RST_STREAM -> render_rst_stream(Data);
+        ?SETTINGS -> render_settings(Data);
+        ?PUSH_PROMISE -> render_push_promise(Data);
+        ?PING -> render_ping(Data);
+        ?GOAWAY -> render_goaway(Data);
+        ?WINDOW_UPDATE -> render_window_update(Data);
+        ?CONTINUATION -> render_continuation(Data);
+        _Unknown -> render_unknown(Data)
+    end,
+    Length = erlang:iolist_size(IoData),
+    Header = <<
+        Length:24,
+        Type:8,
+        Flags:8,
+        _R:1,
+        StreamId:31
+    >>,
+    [Header, IoData].
+
+
+render_data(Data) when is_binary(Data) ->
+    Data.
+
+
+render_headers(Flags, {#priority{} = Priority, Data}) when is_binary(Data) ->
+    case ?IS_FLAG(Flags, Priority) of
+        true ->
+            [h2_priority:render(Priority), Data];
+        false ->
+            Data
+    end.
+
+
+render_priority(#priority{} = Priority) ->
+    h2_priority:render(Priority).
+
+
+render_rst_stream(ErrorCode)
+        when is_integer(ErrorCode), ErrorCode >= 0 ->
+    <<ErrorCode:32>>.
+
+
+render_settings(Settings) when is_list(Settings) ->
+    lists:map(fun({Key, Value}) ->
+        <<Key:16, Value:32/unsigned>>
+    end, Settings).
+
+
+render_push_promise({StreamId, Data}) when is_integer(StreamId), StreamId > 0 ->
+    <<0:1, StreamId:31, Data/binary>>.
+
+
+render_ping(Data) when is_binary(Data), size(Data) == 8 ->
+    Data.
+
+
+render_goaway({LastStreamId, ErrorCode, Debug})
+        when is_integer(LastStreamId), LastStreamId > 0,
+            is_integer(ErrorCode), ErrorCode > 0 ->
+    <<0:1, LastStreamId:31, ErrorCode:32, Debug/binary>>.
+
+
+render_window_update(Increment) when is_integer(Increment), Increment > 0 ->
+    <<0:1, Increment:31>>.
+
+
+render_continuation(Data) when is_binary(Data) ->
+    Data.
+
+
+render_unknown(Data) when is_binary(Data) ->
+    Data.
