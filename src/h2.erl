047be7a88c39aba2fe94c05bc037e31cbae3f552
Paul J. Davis <paul.joseph.davis@gmail.com>
2019-09-23 14:34:47 -0500

    Move includes and types to h2.{erl|hrl}

---
+++ src/h2.erl
@@ -0,0 +1,20 @@
+-module(h2).
+
+
+-include("h2.hrl").
+
+
+-export_type([
+    error_code/0,
+    flag/0,
+
+    stream_id/0,
+
+    priority/0,
+    settings/0,
+
+    frame_type/0,
+    frame_data/0,
+    frame/0
+]).
+
